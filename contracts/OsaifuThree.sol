bbbd4fdbacd87f436001f470ec149a60e496f887
Osaifu3 <hello@osaifu3.app>
2022-10-25 15:44:28 +0800

    [feat] add init.

--- contracts/OsaifuThree.sol
+++ contracts/OsaifuThree.sol
@@ -7,6 +7,16 @@ import "./modules/Signature.sol";
 import "./modules/DiamondCutModule.sol";
 
 contract OsaifuThree is Permission, Signature, DiamondCutModule {
+    uint256 constant currentVersion = 1;
+    uint256 version = 0;
+
+    function Initialize() public {
+        require(version < currentVersion, "Already initialized");
+        __Ownable_init();
+        __EIP712_init("OsaifuThree", "1");
+        version = currentVersion;
+    }
+
     function call(address target, bytes calldata payload) public {
         require(
             delegatee[target].isAuthorized(msg.sender, target, payload),
@@ -16,7 +26,10 @@ contract OsaifuThree is Permission, Signature, DiamondCutModule {
         require(success, string(returnData));
     }
 
-    function callFromOwner(address target, bytes calldata payload) public onlyOwner {
+    function callFromOwner(address target, bytes calldata payload)
+        public
+        onlyOwner
+    {
         (bool success, bytes memory returnData) = target.call(payload);
         require(success, string(returnData));
     }
---
+++ contracts/modules/MetaTxModule.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity >=0.8.0 <0.9.0;
+
+import "./ModuleBase.sol";
+
+// Todo: add 2771 and 4337 compatable meta transaction support
+contract MetaTxModule {}
